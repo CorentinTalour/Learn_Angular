a3715aaeb4cf3a6d0f40650f6cf0ab9c102be851
Corentin Talour <corentin.talour@bts-malraux.net>
2024-06-07 20:21:09 +0200

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NourritureListComponent } from './nourriture-container/nourriture-list/nourriture-list.component';
+import { NourritureDetailsComponent } from './nourriture-container/nourriture-details/nourriture-details.component';
+import { NourritureContainerComponent } from './nourriture-container/nourriture-container.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NourritureListComponent,
+    NourritureDetailsComponent,
+    NourritureContainerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
